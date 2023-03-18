c163a34a33e27a4e79454830898c58f5f2d06fd0
kyrollos2 <125082123+kyrollos2@users.noreply.github.com>
2023-03-18 15:58:26 -0400

    Add files via upload

--- WebScraping.py
+++ WebScraping.py
@@ -3,16 +3,21 @@ import json
 from math import radians, sin, cos, sqrt, atan2
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderUnavailable
-import pymongo
 from pymongo import MongoClient
+from bson import json_util
+
 
 # Geolocator object to get latitude and longitude from city name
 geolocator = Nominatim(user_agent="Air-Quality")
 
 # API key for the Air Quality Open Data Platform
 API_KEY = '128d5dfbee02521f1ef91d6cf86024717e64bedf'
-MongoUrl='mongodb://localhost:27017/'
 
+# MongoDB connection
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['HackKean']
+collection= db['air_quality_data']
 
 def get_aqi():
     # Get city input from user
@@ -37,6 +42,8 @@ def get_aqi():
     # Check if there is an air quality station in the city
     if 'aqi' in response['data']:
         aqi = response['data']['aqi']
+        iaqi = response['data']['iaqi']
+        daily_forecast = response['data']['forecast']['daily']
     else:
         # Find the nearest air quality station
         print("Finding nearest air quality station...")
@@ -59,14 +66,16 @@ def get_aqi():
         print("Retrieving data from nearest station...")
         response = requests.get(f'https://api.waqi.info/feed/geo:{nearest_station["lat"]};{nearest_station["lon"]}/?token={API_KEY}').json()
         aqi = response['data']['aqi']
+        iaqi = response['data']['iaqi']
+        daily_forecast = response['data']['forecast']['daily']
 
     # Print AQI
     print(f'The AQI in {city} is {aqi}')
-client = MongoClient(MongoUrl)
-db = client['HackKean']
-collection = db['Air Quality Information']
 
-
+    # Save data to MongoDB
+    data = {'city': city, 'state': state, 'lat': lat, 'long': long, 'aqi': aqi, 'iaqi': iaqi, 'daily_forecast': daily_forecast}
+    bson_data = json_util.loads(json_util.dumps(data))
+    collection.insert_one(data)
 
 get_aqi()
 
